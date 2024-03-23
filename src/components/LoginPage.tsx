583a02f59a826e56c24b3f3029ad9228a488fd22
Triều Nguyễn <nmtrieu762@gmail.com>
2024-03-23 10:35:39 +0700

    mm

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -49,8 +49,8 @@ const LoginPage: React.FC = () => {
     <section className={styles.gradientCustom}>
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div className={styles.cardBackground} style={{ borderRadius: '1rem' }}>
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">; 
+            <div className={styles.cardBackground} style={{ borderRadius: '1rem', marginTop: '-60px' }}>
               <div className="card-body p-5 text-center">
                 <div className="mb-md-5 mt-md-4 pb-5">
                   <h2 className={`fw-bold mb-2 ${styles.loginTitle}`}>Đăng Nhập</h2>
